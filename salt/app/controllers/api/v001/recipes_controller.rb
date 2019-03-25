f29be736738ae25a014086a1c2c4839a3efe427e
github username <github email address>
2019-03-25 19:19:52 +0000

    adding create to recipe controller

--- salt/app/controllers/api/v001/recipes_controller.rb
+++ salt/app/controllers/api/v001/recipes_controller.rb
@@ -5,4 +5,22 @@ class Api::V001::RecipesController < ApplicationController
     render json: @recipes
   end
 
+  def create 
+    @recipe = Recipe.create(name: recipe_params[:name], instructions: recipe_params[:instructions], image: recipe_params[:image])
+
+    # params[:ingredients].map do |item|  
+    #   @ingredient = Ingredient.find_or_create_by(name: item[:ingredient_name])
+    #   @ingredient.update(category: item[:ingredient_category], have: false)
+    #   @recipe_qty_ingredient = RecipeQtyIngredient.create(recipe_id: @recipe.id, ingredient_id: @ingredient.id, qty: item[:qty], unit: item[:unit])
+    # end 
+    
+    byebug
+  end 
+
+  private 
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :instructions, :image)
+  end 
+
 end
