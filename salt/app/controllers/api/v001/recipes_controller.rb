7f3050025dc95b7bfd9533f3ecdcf8e20597b072
github username <github email address>
2019-03-26 11:30:38 +0000

    recipe form working

--- salt/app/controllers/api/v001/recipes_controller.rb
+++ salt/app/controllers/api/v001/recipes_controller.rb
@@ -6,30 +6,29 @@ class Api::V001::RecipesController < ApplicationController
   end
 
   def create 
-    @user = User.find_by(email: params[:user])
-    @recipe = Recipe.new(recipe_params,user_id: @user.id)
+   
+    @recipe = Recipe.new(recipe_params)
     
     if @recipe.valid?
       @recipe.save
-      # render json: @recipe
-      print("RECIPE COOKED")
+      params[:ingredients].map do |item|  
+        @ingredient = Ingredient.find_or_create_by(name: item[:ingredient_name])
+        @ingredient.update(category: item[:ingredient_category], have: false)
+        @recipe_qty_ingredient = RecipeQtyIngredient.create(recipe_id: @recipe.id, ingredient_id: @ingredient.id, qty: item[:qty], unit: item[:unit])
+      end 
+      render json: @recipe
+      # print(@recipe)
     else
-      # render json: "recipe parameters invalid"
-      print("NO RECIPE")
+       render json: "recipe parameters invalid"
+      # print("NO RECIPE")
     end
-    # params[:ingredients].map do |item|  
-    #   @ingredient = Ingredient.find_or_create_by(name: item[:ingredient_name])
-    #   @ingredient.update(category: item[:ingredient_category], have: false)
-    #   @recipe_qty_ingredient = RecipeQtyIngredient.create(recipe_id: @recipe.id, ingredient_id: @ingredient.id, qty: item[:qty], unit: item[:unit])
-    # end 
-    
-    byebug
+   
   end 
 
   private 
 
   def recipe_params
-    params.require(:recipe).permit(:name, :instructions, :image)
+    params.require(:recipe).permit(:name, :instructions, :image, :user_id)
   end 
 
 end
